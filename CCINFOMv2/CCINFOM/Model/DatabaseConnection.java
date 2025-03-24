a3a08c459d6280d9a6a45017be252cc2a618435d
biacora <166267267+biacora@users.noreply.github.com>
2025-03-24 15:33:01 +0800

    Update DatabaseConnection.java

--- CCINFOMv2/CCINFOM/Model/DatabaseConnection.java
+++ CCINFOMv2/CCINFOM/Model/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/dbapp_bankdb";
+    private static final String URL = "jdbc:mysql://localhost:3307/dbapp_bankdb";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "1234";
 
